800556a5c888d6890b8bd07869c74775f350a010
root <root@localhost.localdomain>
2024-01-02 20:48:20 +0000

    implemented run function

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,7 @@
 pub mod args;
+
+use std::fs;
+
+pub fn run(args: args::Args) {
+  let file_string = fs::read_to_string(args.file_path).expect("Couldn't load file.");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
-use std::fs;
+use my_minigrep::{args::Args, run};
 
-use my_minigrep::args::Args;
 fn main() {
-    let args = Args::get();
-    let file_string = fs::read_to_string(args.file_path).expect("Couldn't load file.");
+  let args = Args::get();
+  run(args);
 }
